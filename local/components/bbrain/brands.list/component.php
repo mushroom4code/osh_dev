048930e6d74569cfcef2f45140ab96bd13dad11b
katy <rodionova@enterego.ru>
2022-12-14 13:14:53 +0300

    - комментарий на получение брендов - получение брендов не по ид, а по имени сущности
    Signed-off-by: katy <rodionova@enterego.ru>

--- local/components/bbrain/brands.list/component.php
+++ local/components/bbrain/brands.list/component.php
@@ -1,46 +1,44 @@
 <?
-if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)
-{
-	die();
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+    die();
 }
+
 use Bitrix\Main\Context,
-	Bitrix\Main\Type\DateTime,
-	Bitrix\Main\Loader,
-	Bitrix\Iblock;
+    Bitrix\Main\Type\DateTime,
+    Bitrix\Main\Loader,
+    Bitrix\Iblock;
 
 \Bitrix\Main\Loader::includeModule("iblock");
 \Bitrix\Main\Loader::includeModule("sale");
 \Bitrix\Main\Loader::includeModule("highloadblock");
 \Bitrix\Main\Loader::includeModule("catalog");
-use Bitrix\Highloadblock as HL; 
 
-		if( !$arParams['IBLOCK_ID'] )
-		{
-			return;
-		}
-	
-				$hlbl = $arParams['IBLOCK_ID']; // Указываем ID нашего highloadblock блока
-				$hlblock = HL\HighloadBlockTable::getById($hlbl)->fetch(); 
-				$entity = HL\HighloadBlockTable::compileEntity($hlblock); 
-				$EHL = $entity->getDataClass(); 
-	
-	
-				$QUERY['select'] = array('*');
-				$QUERY['order'] =  array("UF_NAME" => "ASC");
-				$QUERY['filter'] =  array();
-				
-				$obData = $EHL::getList($QUERY); 	
-				while( $arData = $obData->Fetch())
-				{
-					$arSections = explode("#", $arData['UF_SECTIONS']);
-					foreach( $arSections as $_sect)
-					{
-						$arResult[$_sect][$arData['ID']] = $arData;
-					}
-					
-					
-					
-				}					
-		
-	$this->IncludeComponentTemplate();
+use Bitrix\Highloadblock as HL;
+
+if (!$arParams['IBLOCK_ID']) {
+    return;
+}
+//	TODO - заменить на получение через наименовнеие сущности highloadblock  Enterego\EnteregoHelper::getHeadBlock('BREND', $actualBlockData);
+// TODO - в $actualBlockData - лежит массив запрашиваемых полей - UF_CODE,UF_FILE,UF_NAME
+$hlbl = $arParams['IBLOCK_ID']; // Указываем ID нашего highloadblock блока
+$hlblock = HL\HighloadBlockTable::getById($hlbl)->fetch();
+$entity = HL\HighloadBlockTable::compileEntity($hlblock);
+$EHL = $entity->getDataClass();
+
+
+$QUERY['select'] = array('*');
+$QUERY['order'] = array("UF_NAME" => "ASC");
+$QUERY['filter'] = array();
+
+$obData = $EHL::getList($QUERY);
+while ($arData = $obData->Fetch()) {
+    $arSections = explode("#", $arData['UF_SECTIONS']);
+    foreach ($arSections as $_sect) {
+        $arResult[$_sect][$arData['ID']] = $arData;
+    }
+
+
+}
+
+$this->IncludeComponentTemplate();
 
