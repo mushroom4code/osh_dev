86bb5d6d919278fc69cc4cd477760d093604e450
katy <rodionova@enterego.ru>
2022-12-15 19:22:09 +0300

    блок вопросы - ответы
    Signed-off-by: katy <rodionova@enterego.ru>

--- about/FAQ/index.php
+++ about/FAQ/index.php
@@ -10,80 +10,77 @@ Asset::getInstance()->addJS("https://www.google.com/recaptcha/api.js");
     <div id="faq" class="box_boxes_delivery mt-3 static">
         <h1>FAQ</h1>
         <div class="d-flex flex-column mb-3" id="FAQ">
-            
-<?
-$k = 0;
-$SectionRes = CIBlockSection::GetList(array(), array('IBLOCK_ID'=>24, 'ACTIVE'=>'Y'), false, array("CODE", 'NAME', 'ID', 'IBLOCK_SECTION_ID', 'XML_ID'));
-while($arSection = $SectionRes->GetNext()) {
-?>
-<div class="mb-5">
-	  <h4 class="mb-4"><?=$arSection['NAME']?></h4>
-	  
-	  <div id="<?=$arSection['XML_ID']?>_faq">
-			<div class="accordion box_with_map" id="<?=$arSection['CODE']?>">
-						<?
-							$arFilter = array(
-							'IBLOCK_ID' => 24, 
-							'ACTIVE' => 'Y',
-							'SECTION_ID' => $arSection['ID']
-							);
-						$resU = CIBlockElement::GetList(Array(), $arFilter, false, false);
-						while($rowFaq = $resU->Fetch())
-						{	$k++;
-						?>
-                        <div class="box">
-                            <div class="card_delivery card-header" id="questions_del_<?=$k?>">
-                                <h3 class="mb-0">
-                                    <button class="btn text-left btn_questions d-flex flex-row justify-content-between"
-                                            type="button" data-toggle="collapse" data-target="#collapse_questions_del_<?=$k?>"
-                                            aria-expanded="true" aria-controls="collapse_questions_del_<?=$k?>">
-                                        <div class="mr-4 faq_question"><?=$rowFaq['NAME']?></div>
-                                        <i class="fa fa-angle-down" style="" aria-hidden="true"></i>
-                                    </button>
-                                </h3>
-                            </div>
-                            <div id="collapse_questions_del_<?=$k?>" class="collapse " aria-labelledby="questions_del_<?=$k?>"
-                                 data-parent="#<?=$arSection['CODE']?>">
-                                <div class="card-body d-flex row_section">
-                                    <div>
-                                       <?=$rowFaq['PREVIEW_TEXT']?>
+
+            <?php
+            $k = 0;
+            $SectionRes = CIBlockSection::GetList(array(), array( 'ACTIVE' => 'Y','CODE'=>'FAQ'), false, array("CODE", 'NAME', 'ID', 'IBLOCK_SECTION_ID', 'XML_ID'));
+            while ($arSection = $SectionRes->GetNext()) {
+                ?>
+                <div class="mb-5">
+                    <h4 class="mb-4"><?= $arSection['NAME'] ?></h4>
+
+                    <div id="<?= $arSection['XML_ID'] ?>_faq">
+                        <div class="accordion box_with_map" id="<?= $arSection['CODE'] ?>">
+                            <?php
+                            $arFilter = array(
+                                'CODE'=>'FAQ',
+                                'ACTIVE' => 'Y',
+                                'SECTION_ID' => $arSection['ID']
+                            );
+                            $resU = CIBlockElement::GetList(array(), $arFilter, false, false);
+                            while ($rowFaq = $resU->Fetch()) {
+                                $k++;
+                                ?>
+                                <div class="box">
+                                    <div class="card_delivery card-header" id="questions_del_<?= $k ?>">
+                                        <h3 class="mb-0">
+                                            <button class="btn text-left btn_questions d-flex flex-row justify-content-between"
+                                                    type="button" data-toggle="collapse"
+                                                    data-target="#collapse_questions_del_<?= $k ?>"
+                                                    aria-expanded="true"
+                                                    aria-controls="collapse_questions_del_<?= $k ?>">
+                                                <div class="mr-4 faq_question"><?= $rowFaq['NAME'] ?></div>
+                                                <i class="fa fa-angle-down" style="" aria-hidden="true"></i>
+                                            </button>
+                                        </h3>
+                                    </div>
+                                    <div id="collapse_questions_del_<?= $k ?>" class="collapse "
+                                         aria-labelledby="questions_del_<?= $k ?>"
+                                         data-parent="#<?= $arSection['CODE'] ?>">
+                                        <div class="card-body d-flex row_section">
+                                            <div>
+                                                <?= $rowFaq['PREVIEW_TEXT'] ?>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>	
-						<?
-						}
-						?>			
-		
-			</div>
-	  </div>
-</div>
-<?
-}
+                                <?php
+                            }
+                            ?>
 
-?>			
-			
-
+                        </div>
+                    </div>
+                </div>
+                <?php } ?>
         </div>
-		<?/*
         <h4 class="mb-4"><b> Есть вопросы, пожелания или комментарии? Напиши их здесь!</b></h4>
         <div class="mb-5">
             <form class="form_company" id="support">
-<!--                <input type="hidden" name="recaptcha_token" value="">-->
-<!--                --><?php //echo bitrix_sessid_post(); ?>
-<!--                <input type="hidden" name="captcha_sid" value="--><?//= $arResult["CAPTCHA_CODE"] ?><!--"/>-->
-<!--                <div class="form-group">-->
-<!--                    --><?//= GetMessage("CAPTCHA_REGF_PROMT") ?>
-<!--                    <div class="form-group">-->
-<!--                        <div class="bx-captcha"><img-->
-<!--                                    src="/bitrix/tools/captcha.php?captcha_sid=--><?//= $arResult["CAPTCHA_CODE"] ?><!--"-->
-<!--                                    width="180" height="40" alt="CAPTCHA"/></div>-->
-<!--                    </div>-->
-<!--                    <div class="form-group">-->
-<!--                        <input type="text" class="form-control" name="captcha_word" maxlength="50"-->
-<!--                               value="" autocomplete="off"/>-->
-<!--                    </div>-->
-<!--                </div>-->
+                <input type="hidden" name="recaptcha_token" value="">
+                <?php echo bitrix_sessid_post(); ?>
+                <input type="hidden" name="captcha_sid" value="<?= $arResult["CAPTCHA_CODE"] ?>"/>
+                <div class="form-group">
+                    <?= GetMessage("CAPTCHA_REGF_PROMT") ?>
+                    <div class="form-group">
+                        <div class="bx-captcha"><img
+                                    src="/bitrix/tools/captcha.php?captcha_sid=<?= $arResult["CAPTCHA_CODE"] ?>"
+                                    width="180" height="40" alt="CAPTCHA"/></div>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="captcha_word" maxlength="50"
+                               value="" autocomplete="off"/>
+                    </div>
+                </div>
                 <div class="form-group mb-3">
                     <label class="label_company">Поделись с нами!</label>
                 </div>
@@ -91,7 +88,8 @@ while($arSection = $SectionRes->GetNext()) {
                     <input type="text" class="form-control input_lk" id="Name" placeholder="ФИО">
                 </div>
                 <div class="form-group mb-3">
-                    <input type="text" data-name="PHONE" class="form-control input_lk" id="phoneNumber" placeholder="Номер телефона">
+                    <input type="text" data-name="PHONE" class="form-control input_lk" id="phoneNumber"
+                           placeholder="Номер телефона">
                 </div>
                 <div class="form-group mb-5">
                     <textarea class="form-control input_lk" id="text" placeholder="Ваш комментарий"></textarea>
@@ -106,7 +104,6 @@ while($arSection = $SectionRes->GetNext()) {
                 </div>
             </form>
         </div>
-		
-		*/?>
+
     </div>
 <?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
