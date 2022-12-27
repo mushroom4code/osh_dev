07b23d03ee1c806cb5beb57e3862882db90e6d2f
katy <rodionova@enterego.ru>
2022-12-27 19:13:26 +0300

    #000018648,#000018667
    Signed-off-by: katy <rodionova@enterego.ru>

--- local/ajax/form_feed.php
+++ local/ajax/form_feed.php
@@ -4,12 +4,25 @@ $NAME = htmlspecialcharsbx($_REQUEST['NAME']);
 $MESSAGE = htmlspecialcharsbx($_REQUEST['MESSAGE']);
 $PHONE = htmlspecialcharsbx($_REQUEST['PHONE']);
 
+use Bitrix\Main\Mail\Event;
 $message = '';
 if (!empty($PHONE) && !empty($MESSAGE)) {
-    $message .= 'Новое сообщение от ' . PHP_EOL . $NAME . PHP_EOL . PHP_EOL;
-    $message .= 'Комментарий пользователя: ' . PHP_EOL . $MESSAGE . PHP_EOL . PHP_EOL;
-    $message .= 'Номер телефона пользователя: ' . PHP_EOL . $PHONE . PHP_EOL . PHP_EOL;
+    $message .= 'Новое сообщение от <b>' . PHP_EOL . $NAME . '</b>.' . PHP_EOL . PHP_EOL;
+    $message .= 'Комментарий пользователя: <b>' . PHP_EOL . $MESSAGE . '</b>' . PHP_EOL . PHP_EOL;
+    $message .= 'Номер телефона пользователя: <b>' . PHP_EOL . $PHONE . '</b>.' . PHP_EOL . PHP_EOL;
     $message .= 'Сайт с которого было отправлено сообщение https://' . $_SERVER['HTTP_HOST'] . '/';
+
+    Event::send(array(
+        "EVENT_NAME" => "FEEDBACK_FORM",
+        "LID" => "s1",
+        "C_FIELDS" => array(
+            "EMAIL" => "rodionova@enterego.ru",
+            "AUTHOR" => $NAME,
+            "AUTHOR_PHONE" => $PHONE,
+            "TEXT" => $MESSAGE,
+            "USER_ID" => 42
+        ),
+    ));
 
     echo mail('rodionova@enterego.ru', 'Новый сайт', $message);
 } else {
