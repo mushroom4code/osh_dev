5fed56c0d37f6d4c3a6f81c9191a926f34a9a2d6
Pavel Vladimirovich <sawel@yandex.ru>
2023-01-10 14:33:42 +0800

    18618 - модуль пвз 1.0

--- bitrix/modules/enterego.pvz/lib/CommonPVZ/lang/ru/handler.php
+++ bitrix/modules/enterego.pvz/lib/CommonPVZ/lang/ru/handler.php
@@ -1,6 +1,6 @@
 <?
-$MESS["COMMONPVZ_TITLE"] = "Выбрать ближайший пункт выдачи2";
-$MESS["COMMONPVZ_DESCRIPTION"] = "Можно выбрать удобный пункт выдачи заказа2";
+$MESS["COMMONPVZ_TITLE"] = "Выбрать ближайший пункт выдачи заказа";
+$MESS["COMMONPVZ_DESCRIPTION"] = "Можно выбрать удобный пункт выдачи заказа на карте";
 $MESS["COMMONPVZ_BTN_CHOOSE"] = "Выбрать пункт выдачи заказа";
 $MESS["COMMONPVZ_CHOOSE_PVZ"] = "Необходимо выбрать пункт выдачи!";
 
