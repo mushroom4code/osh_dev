c8e82e191d07f942ed6639e492371eabda6e86a7
maxim.vagatkin <vagatkin@enterego.ru>
2023-01-13 18:33:10 +0300

    иправление работы поиска и быстрый поиск

--- local/templates/Oshisha/components/bitrix/search.title/oshisha_search.title/ajax.php
+++ local/templates/Oshisha/components/bitrix/search.title/oshisha_search.title/ajax.php
@@ -1,34 +1,68 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
-<?
-if(!empty($arResult["CATEGORIES"]) && $arResult['CATEGORIES_ITEMS_EXISTS']):?>
-	<table class="title-search-result">
-		<?foreach($arResult["CATEGORIES"] as $category_id => $arCategory):?>
-			<tr>
-				<th class="title-search-separator">&nbsp;</th>
-				<td class="title-search-separator">&nbsp;</td>
-			</tr>
-			<?foreach($arCategory["ITEMS"] as $i => $arItem):?>
-			<tr>
-				<?if($i == 0):?>
-					<th>&nbsp;<?echo $arCategory["TITLE"]?></th>
-				<?else:?>
-					<th>&nbsp;</th>
-				<?endif?>
-
-				<?if($category_id === "all"):?>
-					<td class="title-search-all"><a href="<?echo $arItem["URL"]?>"><?echo $arItem["NAME"]?></a></td>
-				<?elseif(isset($arItem["ICON"])):?>
-					<td class="title-search-item"><a href="<?echo $arItem["URL"]?>"><img src="<?echo $arItem["ICON"]?>"><?echo $arItem["NAME"]?></a></td>
-				<?else:?>
-					<td class="title-search-more"><a href="<?echo $arItem["URL"]?>"><?echo $arItem["NAME"]?></a></td>
-				<?endif;?>
-			</tr>
-			<?endforeach;?>
-		<?endforeach;?>
-		<tr>
-			<th class="title-search-separator">&nbsp;</th>
-			<td class="title-search-separator">&nbsp;</td>
-		</tr>
-	</table><div class="title-search-fader"></div>
-<?endif;
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+if (empty($arResult["CATEGORIES"]))
+    return;
 ?>
+<div class="bx_searche">
+    <?foreach($arResult["CATEGORIES"] as $category_id => $arCategory):?>
+        <?foreach($arCategory["ITEMS"] as $i => $arItem):?>
+            <?//echo $arCategory["TITLE"]?>
+            <?if($category_id === "all"):?>
+                <div class="bx_item_block" style="min-height:0">
+                    <div class="bx_img_element"></div>
+                    <div class="bx_item_element"><hr></div>
+                </div>
+                <div class="bx_item_block all_result">
+                    <div class="bx_img_element"></div>
+                    <div class="bx_item_element">
+                        <span class="all_result_title"><a href="<?echo $arItem["URL"]?>"><?echo $arItem["NAME"]?></a></span>
+                    </div>
+                    <div style="clear:both;"></div>
+                </div>
+            <?elseif(isset($arResult["ELEMENTS"][$arItem["ITEM_ID"]])):
+                $arElement = $arResult["ELEMENTS"][$arItem["ITEM_ID"]];?>
+                <div class="bx_item_block">
+                    <?if (is_array($arElement["PICTURE"])):?>
+                        <div class="bx_img_element">
+                            <div class="bx_image" style="background-image: url('<?echo $arElement["PICTURE"]["src"]?>')"></div>
+                        </div>
+                    <?endif;?>
+                    <div class="bx_item_element">
+                        <a href="<?echo $arItem["URL"]?>"><?echo $arItem["NAME"]?></a>
+                        <?
+                        foreach($arElement["PRICES"] as $code=>$arPrice)
+                        {
+                            if ($arPrice["MIN_PRICE"] != "Y")
+                                continue;
+
+                            if($arPrice["CAN_ACCESS"])
+                            {
+                                if($arPrice["DISCOUNT_VALUE"] < $arPrice["VALUE"]):?>
+                                    <div class="bx_price">
+                                        <?=$arPrice["PRINT_DISCOUNT_VALUE"]?>
+                                        <span class="old"><?=$arPrice["PRINT_VALUE"]?></span>
+                                    </div>
+                                <?else:?>
+                                    <div class="bx_price"><?=$arPrice["PRINT_VALUE"]?></div>
+                                <?endif;
+                            }
+                            if ($arPrice["MIN_PRICE"] == "Y")
+                                break;
+                        }
+                        ?>
+                    </div>
+                    <div style="clear:both;"></div>
+                </div>
+            <?else:?>
+                <div class="bx_item_block others_result">
+                    <div class="bx_img_element">
+                        <div class="bx_image" style="background-image: url('<?echo $arItem["ICON"]?>')"></div>
+                    </div>
+                    <div class="bx_item_element">
+                        <a href="<?echo $arItem["URL"]?>"><?echo $arItem["NAME"]?></a>
+                    </div>
+                    <div style="clear:both;"></div>
+                </div>
+            <?endif;?>
+        <?endforeach;?>
+    <?endforeach;?>
+</div>
