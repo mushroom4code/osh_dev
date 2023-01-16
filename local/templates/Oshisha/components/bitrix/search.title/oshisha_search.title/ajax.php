f48dce288f71cf69e02f3149561fd7df0c89515e
maxim.vagatkin <vagatkin@enterego.ru>
2023-01-16 17:30:45 +0300

    доработка поиска

--- local/templates/Oshisha/components/bitrix/search.title/oshisha_search.title/ajax.php
+++ local/templates/Oshisha/components/bitrix/search.title/oshisha_search.title/ajax.php
@@ -3,16 +3,16 @@ if (empty($arResult["CATEGORIES"]))
     return;
 ?>
 <div class="bx_searche">
+    <? $test = GetMessage("ALL_RESULTS");?>
     <?foreach($arResult["CATEGORIES"] as $category_id => $arCategory):?>
         <?foreach($arCategory["ITEMS"] as $i => $arItem):?>
             <?//echo $arCategory["TITLE"]?>
             <?if($category_id === "all"):?>
-                <div class="bx_item_block" style="min-height:0">
-                    <div class="bx_img_element"></div>
-                    <div class="bx_item_element"><hr></div>
-                </div>
-                <div class="bx_item_block all_result">
-                    <div class="bx_img_element"></div>
+<!--                <div class="bx_item_block" style="min-height:0">-->
+<!--                    <div class="bx_img_element"></div>-->
+<!--                    <div class="bx_item_element"><hr></div>-->
+<!--                </div>-->
+                <div class="bx_item_block all_result" onclick="window.location='<?echo $arItem["URL"]?>';">
                     <div class="bx_item_element">
                         <span class="all_result_title"><a href="<?echo $arItem["URL"]?>"><?echo $arItem["NAME"]?></a></span>
                     </div>
@@ -20,7 +20,7 @@ if (empty($arResult["CATEGORIES"]))
                 </div>
             <?elseif(isset($arResult["ELEMENTS"][$arItem["ITEM_ID"]])):
                 $arElement = $arResult["ELEMENTS"][$arItem["ITEM_ID"]];?>
-                <div class="bx_item_block">
+                <div class="bx_item_block" onclick="window.location='<?echo $arItem["URL"]?>';">
                     <?if (is_array($arElement["PICTURE"])):?>
                         <div class="bx_img_element">
                             <div class="bx_image" style="background-image: url('<?echo $arElement["PICTURE"]["src"]?>')"></div>
@@ -49,16 +49,6 @@ if (empty($arResult["CATEGORIES"]))
                                 break;
                         }
                         ?>
-                    </div>
-                    <div style="clear:both;"></div>
-                </div>
-            <?else:?>
-                <div class="bx_item_block others_result">
-                    <div class="bx_img_element">
-                        <div class="bx_image" style="background-image: url('<?echo $arItem["ICON"]?>')"></div>
-                    </div>
-                    <div class="bx_item_element">
-                        <a href="<?echo $arItem["URL"]?>"><?echo $arItem["NAME"]?></a>
                     </div>
                     <div style="clear:both;"></div>
                 </div>
