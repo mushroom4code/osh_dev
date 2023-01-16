90695c3f4a2da071ace563246aa03c249bb2d0ef
Pavel Vladimirovich <sawel@yandex.ru>
2023-01-16 23:52:29 +0800

    18618 - модуль пвз 1.1

--- bitrix/modules/enterego.pvz/lib/CommonPVZ/__config_deliveries.php
+++ bitrix/modules/enterego.pvz/lib/CommonPVZ/__config_deliveries.php
@@ -7,3 +7,6 @@ $CONFIG_DELIVERIES['PickPoint']['ikn'] = '9990000112';
 
 $CONFIG_DELIVERIES['FivePost']['login'] = 'smaksultan';
 $CONFIG_DELIVERIES['FivePost']['password'] = 'C78CCD5137422CCE292210C7F1AADF57284D7320';
+
+$CONFIG_DELIVERIES['SDEK']['setAccount'] = 'Qzidnsik5xW7cyJ86Vu89zwshwTq6iQF';
+$CONFIG_DELIVERIES['SDEK']['setSecure'] = 'eyQN5S9Kvn5xcJWIHjWQFMGxVH6FfMbZ';
