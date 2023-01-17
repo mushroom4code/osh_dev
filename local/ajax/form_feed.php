066665fbd470a6ca6568d67fdd830229b751859e
Dmitry Kragel <kragel@enterego.ru>
2023-01-17 14:34:55 +0300

    добавление файлов и чек согласия с перс данными

--- local/ajax/form_feed.php
+++ local/ajax/form_feed.php
@@ -1,12 +1,33 @@
 <?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
 
+use Bitrix\Main\Mail\Event;
+use Bitrix\Main\Loader;
+use B01110011ReCaptcha\BitrixCaptcha;
+
+CModule::IncludeModule("iblock");
+Loader::includeModule('main');
+
 $NAME = htmlspecialcharsbx($_REQUEST['NAME']);
 $MESSAGE = htmlspecialcharsbx($_REQUEST['MESSAGE']);
 $PHONE = htmlspecialcharsbx($_REQUEST['PHONE']);
-
-use Bitrix\Main\Mail\Event;
 $message = '';
+
 if (!empty($PHONE) && !empty($MESSAGE)) {
+
+    $el = new CIBlockElement();
+    $arElement = [
+        'ACTIVE' => 'Y',
+        'IBLOCK_ID' => IBLOCK_NEW_SITE_COMENTS,
+        'NAME' => $NAME,
+        'DETAIL_TEXT' => $MESSAGE,
+        'PROPERTY_VALUES' => [
+            'USER_NAME' => $NAME,
+            'PHONE' => $PHONE,
+            'USER_FILES' => $_FILES,
+        ],
+    ];
+    $result = $el->add($arElement);
+
     $message .= 'Новое сообщение от' . PHP_EOL . $NAME  . PHP_EOL . PHP_EOL;
     $message .= 'Комментарий пользователя:' . PHP_EOL . $MESSAGE  . PHP_EOL . PHP_EOL;
     $message .= 'Номер телефона пользователя:' . PHP_EOL . $PHONE . PHP_EOL . PHP_EOL;
