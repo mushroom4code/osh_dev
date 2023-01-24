89aff3d33e0512641a5c53909b4c1dc42a4ab185
katya <rodionova@enterego.ru>
2023-01-24 19:11:29 +0300

    #000018891 - правки z-index
    Signed-off-by: katya <rodionova@enterego.ru>

--- local/templates/Oshisha/include/feedback-popup-line.php
+++ local/templates/Oshisha/include/feedback-popup-line.php
@@ -2,7 +2,7 @@
     $(document).ready(function () {
         let new_site = sessionStorage.getItem('new_site');
         if (new_site !== 'true') {
-            $(document).find('header').append('<div class="position-absolute width-fit-content container_header z-index-1000 margin-rel">' +
+            $(document).find('header').append('<div class="position-absolute container_header z-index-1000 margin-rel">' +
                 '<div class="bg-gray-white text-center p-lg-4 p-md-4 p-3 max-width-400 br-10 new-site-feedback position-relative">' +
                 '<p class="mb-0 text_font_13 font-weight-bold">' +
                 'Наш новый сайт находится на этапе разработки, мы будем рады вашим отзывам!</p>' +
