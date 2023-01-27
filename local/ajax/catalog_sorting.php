fb930544f41d54e6b66853290b9a994b2da61470
Dmitry Kragel <kragel@enterego.ru>
2023-01-27 13:53:42 +0300

    #000018706 удалены лишние файлы, работает подсветка вкусов, переделан скрипт обновления оценок популярности

--- local/ajax/catalog_sorting.php
+++
@@ -1,5 +0,0 @@
-<?php
-require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
-if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
-
-if (!CModule::IncludeModule('sale')) exit();
