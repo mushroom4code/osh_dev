d6c6b97fc74247048af7991716522bcd472e3792
Dmitry Kragel <kragel@enterego.ru>
2023-01-31 19:15:20 +0300

    #000018706 подключена пагинация

--- bitrix/php_interface/enterego_class/modules/updateMinSortPrice.php
+++ bitrix/php_interface/enterego_class/modules/updateMinSortPrice.php
@@ -1,5 +1,4 @@
-<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php');
-
+<?php
 function updateMinSortPrice()
 {
     CModule::IncludeModule('catalog') || die();
