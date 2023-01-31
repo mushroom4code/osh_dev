f8fb26dedb17cc1a89bb12c1a30f4378054645bc
katya <rodionova@enterego.ru>
2023-01-31 14:21:41 +0300

    #000018953 - ссылка в меню для неавторизованных пользователей
    Signed-off-by: katya <rodionova@enterego.ru>

--- .bottom.menu.php
+++ .bottom.menu.php
@@ -1,50 +1,29 @@
 <?
-$aMenuLinks = Array(
-	Array(
-		"О нас", 
-		"/about/o-nas/", 
-		Array(), 
-		Array(), 
-		"" 
-	),
-	Array(
-		"Доставка и оплата", 
-		"/about/delivery/", 
-		Array(), 
-		Array(), 
-		"" 
-	),
-	Array(
-		"Для бизнеса", 
-		"/about/for-business/", 
-		Array(), 
-		Array(), 
-		"" 
-	),
-	
-	
-	
-	/*
-	Array(
-		"Контакты", 
-		"/about/contacts/", 
-		Array(), 
-		Array(), 
-		"" 
-	),
+$ar = [];
+$aMenuLinks = array(
+    array(
+        "О нас",
+        "/about/o-nas/",
+        array(),
+        array(),
+        ""
+    ),
+    array(
+        "Для бизнеса",
+        "/about/for-business/",
+        array(),
+        array(),
+        ""
+    ),
 
-	Array(
-		"Пользовательское соглашение", 
-		"/about/users_rules/", 
-		Array(), 
-		Array(), 
-		"" 
-	),
-	Array(
-		"Политика конфиденциальности", 
-		"/about/politics/", 
-		Array(), 
-		Array(), 
-		"" 
-	)*/
 );
+
+if ($USER->IsAuthorized()) {
+    $aMenuLinks[] = array(
+        "Доставка и оплата",
+        "/about/delivery/",
+        array(),
+        array(),
+        "",
+    );
+}
