7a81c9fe1353d392f439eeb1342c8aecf625afa5
katya <rodionova@enterego.ru>
2023-02-07 13:56:57 +0300

    #000018745 - раскрывающийся список
    Signed-off-by: katya <rodionova@enterego.ru>

--- local/components/bbrain/brands.list/templates/.default/template.php
+++ local/components/bbrain/brands.list/templates/.default/template.php
@@ -8,37 +8,41 @@ $arParamsString = array(
     "IBLOCK_ID" => $arParams['IBLOCK_ID'],
     'SEF_URL' => $arParams['SEF_URL'],
     'FIRST_ID' => $arParams['FIRST_ID'],
-);
+); ?>
 
-?>
-
-<div class="mb-5 static" id="box_brands">
+<div class="mb-5 static" id="box_boxes">
     <h1>Бренды</h1>
-    <?
-    $rsSections = CIBlockSection::GetList(array('SORT' => 'ASC'), array('IBLOCK_ID' => IBLOCK_CATALOG, 'ACTIVE' => 'Y', 'GLOBAL_ACTIVE' => 'Y', 'DEPTH_LEVEL' => 1), false, array('DEPTH_LEVEL', 'ID', 'IBLOCK_ID', 'IBLOCK_SECTION_ID', 'NAME'));
+    <?php $rsSections = CIBlockSection::GetList(
+        array('SORT' => 'ASC'),
+        array('IBLOCK_ID' => IBLOCK_CATALOG, 'ACTIVE' => 'Y', 'GLOBAL_ACTIVE' => 'Y', 'DEPTH_LEVEL' => 1),
+        false,
+        array('DEPTH_LEVEL', 'ID', 'IBLOCK_ID', 'IBLOCK_SECTION_ID', 'NAME'));
 
     while ($arSection = $rsSections->GetNext()) {
-        if (empty($arResult[$arSection['NAME']])) continue;
-        ?>
-        <h5 class="mt-5 mb-3"><?= $arSection['NAME'] ?></h5>
-        <div class="box_brands ">
-            <div class="box_with_brands_parents">
+        if (empty($arResult[$arSection['NAME']]))
+            continue; ?>
+        <div class="d-flex flex-column mb-5" id="box_brands">
+            <h5 class="mt-5 mb-3"><?= $arSection['NAME'] ?></h5>
+            <div class="box_with_brands_parents" id="click_brand_<?= $arSection['ID']; ?>">
                 <? foreach ($arResult[$arSection['NAME']] as $_brands): ?>
                     <div class="box_with_brands d-flex justify-content-center align-items-center col col-sm">
-                        <? if ($_brands['UF_FILE']):?>
+                        <? if ($_brands['UF_FILE']): ?>
                             <? $UF_FILE = CFile::ResizeImageGet($_brands['UF_FILE'], array('width' => 150, 'height' => 55), BX_RESIZE_IMAGE_PROPORTIONAL, true) ?>
                             <a href="/brands/<?= $_brands['UF_CODE'] ?>/" class="logo_brand">
                                 <img src="<?= $UF_FILE['src'] ?>">
                             </a>
-                        <? else:?>
+                        <? else: ?>
                             <a href="/brands/<?= $_brands['UF_CODE'] ?>/"
                                class="d-flex justify-content-center align-items-center"><?= $_brands['UF_NAME'] ?></a>
-                        <?endif; ?>
+                        <? endif; ?>
                     </div>
-                <?endforeach; ?>
+                <? endforeach; ?>
+            </div>
+            <div class="d-flex justify-content-end m-2 box_button">
+                <a href="javascript:void(0)" class="link_menu_catalog link_red_button link_brand" id="click_brand_<?=
+                $arSection['ID']; ?>">Посмотреть все</a>
             </div>
         </div>
-
         <?
     }
 
