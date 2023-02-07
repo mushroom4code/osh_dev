28444767ef40762c83288e671bdeec18730f6a3b
katya <rodionova@enterego.ru>
2023-02-07 14:26:38 +0300

    #000018745 - рефакторинг кода
    Signed-off-by: katya <rodionova@enterego.ru>

--- local/components/bbrain/brands.list/templates/.default/template.php
+++ local/components/bbrain/brands.list/templates/.default/template.php
@@ -9,7 +9,6 @@ $arParamsString = array(
     'SEF_URL' => $arParams['SEF_URL'],
     'FIRST_ID' => $arParams['FIRST_ID'],
 ); ?>
-
 <div class="mb-5 static" id="box_boxes">
     <h1>Бренды</h1>
     <?php $rsSections = CIBlockSection::GetList(
@@ -19,8 +18,9 @@ $arParamsString = array(
         array('DEPTH_LEVEL', 'ID', 'IBLOCK_ID', 'IBLOCK_SECTION_ID', 'NAME'));
 
     while ($arSection = $rsSections->GetNext()) {
-        if (empty($arResult[$arSection['NAME']]))
-            continue; ?>
+        if (empty($arResult[$arSection['NAME']])) {
+            continue;
+        } ?>
         <div class="d-flex flex-column mb-5" id="box_brands">
             <div class="d-flex flex-row">
                 <h5 class="mt-5 mb-3"><?= $arSection['NAME'] ?></h5>
@@ -32,23 +32,23 @@ $arParamsString = array(
                 </div>
             </div>
             <div class="box_with_brands_parents" id="click_brand_<?= $arSection['ID']; ?>">
-                <? foreach ($arResult[$arSection['NAME']] as $_brands): ?>
+                <?php foreach ($arResult[$arSection['NAME']] as $_brands) { ?>
                     <div class="box_with_brands d-flex justify-content-center align-items-center col col-sm">
-                        <? if ($_brands['UF_FILE']): ?>
-                            <? $UF_FILE = CFile::ResizeImageGet($_brands['UF_FILE'], array('width' => 150, 'height' => 55), BX_RESIZE_IMAGE_PROPORTIONAL, true) ?>
+                        <?php if ($_brands['UF_FILE']) {
+                            $UF_FILE = CFile::ResizeImageGet(
+                                $_brands['UF_FILE'],
+                                array('width' => 150, 'height' => 55),
+                                BX_RESIZE_IMAGE_PROPORTIONAL, true); ?>
                             <a href="/brands/<?= $_brands['UF_CODE'] ?>/" class="logo_brand">
                                 <img src="<?= $UF_FILE['src'] ?>">
                             </a>
-                        <? else: ?>
+                        <?php } else { ?>
                             <a href="/brands/<?= $_brands['UF_CODE'] ?>/"
                                class="d-flex justify-content-center align-items-center"><?= $_brands['UF_NAME'] ?></a>
-                        <? endif; ?>
+                        <?php } ?>
                     </div>
-                <? endforeach; ?>
+                <?php } ?>
             </div>
         </div>
-        <?
-    }
-
-    ?>
+    <?php } ?>
 </div>
