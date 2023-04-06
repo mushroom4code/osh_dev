50c55fd57673cefe1fe5caf7b7fe16bb725a794a
karpenko andrey <ak@enterego.ru>
2023-04-06 19:38:12 +0300

    #000019234 -  add API for find product by name and return base price

---
+++ parser/index.php
@@ -0,0 +1,65 @@
+<?php
+
+use Bitrix\Catalog\Model\Price;
+use Bitrix\Main\Application;
+use Bitrix\Main\Loader;
+
+require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+if (!Loader::includeModule('catalog')) {
+    die();
+}
+
+if (!Loader::includeModule('search')) {
+    die();
+}
+
+$request = Application::getInstance()->getContext()->getRequest();
+$token = $request->getHeader('token');
+if (empty($token) || $token !== OSHISHA_PARSER_TOKEN) {
+    exit();
+}
+
+$productName  = $request->get('name');
+$arParams = [0=>[['=MODULE_ID'=>'iblock', 'PARAM1'=>'1c_catalog'], 'LOGIC'=>'OR']];
+$obSearch = new CSearch();
+
+//When restart option is set we will ignore error on query with only stop words
+$obSearch->SetOptions(array(
+    "ERROR_ON_EMPTY_STEM" => $arParams["RESTART"] != "Y",
+    "NO_WORD_LOGIC" => $arParams["NO_WORD_LOGIC"] == "Y",
+));
+
+
+$exFILTER = [['=MODULE_ID'=>'iblock', 'PARAM1'=>'1c_catalog', 'PARAM2'=>[12]]];
+$arFilter = [
+    'SITE_ID'=>SITE_ID,
+    'QUERY'=>$productName,
+    'TAGS'=>false,
+    'CHECK_DATE'=>'Y'
+];
+
+$_arPhrase = stemming_split($productName, LANGUAGE_ID);
+$obSearch->Search($arFilter, [], $exFILTER);
+foreach ($obSearch->arResult as $item) {
+    $res = CIBlockElement::GetByID($item['item']);
+
+    if ($arRes = $res->GetNext()) {
+        $resPrice = Price::getList(['filter'=>[
+            'PRODUCT_ID' => $arRes['ID'],
+            'CATALOG_GROUP_ID' => BASIC_PRICE,
+        ]]);
+
+        if ($arResPrice = $resPrice->fetch()) {
+            print_r(json_encode([
+                'name' => $arRes['NAME'],
+                'id' => $arRes['ID'],
+                'price' => $arResPrice['PRICE']
+                ]));
+            exit();
+        }
+    }
+}
+
+exit();
+
+
