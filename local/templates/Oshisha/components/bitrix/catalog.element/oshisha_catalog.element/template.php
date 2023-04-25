cbbffffa0075205333b052ad8e11f2651d399c38
kataya <rodionova@enterego.ru>
2023-04-25 13:58:02 +0300

    #000019561 - правка в обмене на описание товара

--- local/templates/Oshisha/components/bitrix/catalog.element/oshisha_catalog.element/template.php
+++ local/templates/Oshisha/components/bitrix/catalog.element/oshisha_catalog.element/template.php
@@ -123,7 +123,7 @@ $price = $actualItem['PRICES_CUSTOM'];
 if (intval($SETTINGS['MAX_QUANTITY']) > 0 && $SETTINGS['MAX_QUANTITY'] < $actualItem['PRODUCT']['QUANTITY'])
     $actualItem['PRODUCT']['QUANTITY'] = $SETTINGS['MAX_QUANTITY'];
 
-$showDescription = !empty($arResult['PREVIEW_TEXT']) || !empty($arResult['DETAIL_TEXT']);
+$showDescription = !empty($arResult['DETAIL_TEXT']);
 $showBuyBtn = in_array('BUY', $arParams['ADD_TO_BASKET_ACTION']);
 $buyButtonClassName = in_array('BUY', $arParams['ADD_TO_BASKET_ACTION_PRIMARY']) ? 'btn-primary' : 'btn-link';
 $showAddBtn = in_array('ADD', $arParams['ADD_TO_BASKET_ACTION']);
@@ -692,11 +692,7 @@ global $option_site;
         <div class="tab-content mt-5">
             <?php if ($showDescription) { ?>
                 <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
-                    <?php if ($arResult['PREVIEW_TEXT'] != '' && ($arParams['DISPLAY_PREVIEW_TEXT_MODE'] === 'S'
-                            || ($arParams['DISPLAY_PREVIEW_TEXT_MODE'] === 'E' && $arResult['DETAIL_TEXT'] == ''))) {
-                        echo $arResult['PREVIEW_TEXT_TYPE'] === 'html' ? $arResult['PREVIEW_TEXT'] : '<p>' . $arResult['PREVIEW_TEXT'] . '</p>';
-                    }
-
+                    <?php
                     if ($arResult['DETAIL_TEXT'] != '') {
                         echo $arResult['DETAIL_TEXT_TYPE'] === 'html' ? $arResult['DETAIL_TEXT'] : '<p>' . $arResult['DETAIL_TEXT'] . '</p>';
                     } ?>
