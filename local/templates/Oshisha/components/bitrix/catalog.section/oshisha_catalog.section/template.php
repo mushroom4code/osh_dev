5a6c45238041ff5c160c13ef130310e0c6786d9f
maxim.vagatkin <vagatkin@enterego.ru>
2023-07-13 18:48:39 +0300

    #000019317 - fix after merge

--- local/templates/Oshisha/components/bitrix/catalog.section/oshisha_catalog.section/template.php
+++ local/templates/Oshisha/components/bitrix/catalog.section/oshisha_catalog.section/template.php
@@ -37,194 +37,196 @@ if (\Enterego\EnteregoHitsHelper::checkIfHits($APPLICATION)) {
         array_merge($arParams, $arResult),
         false
     );
+
+    return;
+} 
+
+if (!empty($arResult['NAV_RESULT'])) {
+    $navParams = array(
+        'NavPageCount' => $arResult['NAV_RESULT']->NavPageCount,
+        'NavPageNomer' => $arResult['NAV_RESULT']->NavPageNomer,
+        'NavNum' => $arResult['NAV_RESULT']->NavNum
+    );
 } else {
-
-    if (!empty($arResult['NAV_RESULT'])) {
-        $navParams = array(
-            'NavPageCount' => $arResult['NAV_RESULT']->NavPageCount,
-            'NavPageNomer' => $arResult['NAV_RESULT']->NavPageNomer,
-            'NavNum' => $arResult['NAV_RESULT']->NavNum
-        );
-    } else {
-        $navParams = array(
-            'NavPageCount' => 1,
-            'NavPageNomer' => 1,
-            'NavNum' => $this->randString()
-        );
-    }
-
-    $showTopPager = false;
-    $showBottomPager = false;
-    $showLazyLoad = false;
-
-    if ($arParams['PAGE_ELEMENT_COUNT'] > 0 && $navParams['NavPageCount'] > 1) {
-        $showTopPager = $arParams['DISPLAY_TOP_PAGER'];
-        $showBottomPager = $arParams['DISPLAY_BOTTOM_PAGER'];
-        $showLazyLoad = $arParams['LAZY_LOAD'] === 'Y' && $navParams['NavPageNomer'] != $navParams['NavPageCount'];
-    }
-
-    $templateLibrary = array('popup', 'ajax', 'fx');
-    $currencyList = '';
-
-    if (!empty($arResult['CURRENCIES'])) {
-        $templateLibrary[] = 'currency';
-        $currencyList = CUtil::PhpToJSObject($arResult['CURRENCIES'], false, true, true);
-    }
-
-    $templateData = array(
-        'TEMPLATE_LIBRARY' => $templateLibrary,
-        'CURRENCIES' => $currencyList
+    $navParams = array(
+        'NavPageCount' => 1,
+        'NavPageNomer' => 1,
+        'NavNum' => $this->randString()
     );
-    unset($currencyList, $templateLibrary);
+}
 
-    $elementEdit = CIBlock::GetArrayByID($arParams['IBLOCK_ID'], 'ELEMENT_EDIT');
-    $elementDelete = CIBlock::GetArrayByID($arParams['IBLOCK_ID'], 'ELEMENT_DELETE');
-    $elementDeleteParams = array('CONFIRM' => GetMessage('CT_BCS_TPL_ELEMENT_DELETE_CONFIRM'));
+$showTopPager = false;
+$showBottomPager = false;
+$showLazyLoad = false;
 
-    $positionClassMap = array(
-        'left' => 'product-item-label-left',
-        'center' => 'product-item-label-center',
-        'right' => 'product-item-label-right',
-        'bottom' => 'product-item-label-bottom',
-        'middle' => 'product-item-label-middle',
-        'top' => 'product-item-label-top'
-    );
+if ($arParams['PAGE_ELEMENT_COUNT'] > 0 && $navParams['NavPageCount'] > 1) {
+    $showTopPager = $arParams['DISPLAY_TOP_PAGER'];
+    $showBottomPager = $arParams['DISPLAY_BOTTOM_PAGER'];
+    $showLazyLoad = $arParams['LAZY_LOAD'] === 'Y' && $navParams['NavPageNomer'] != $navParams['NavPageCount'];
+}
 
-    $discountPositionClass = '';
-    if ($arParams['SHOW_DISCOUNT_PERCENT'] === 'Y' && !empty($arParams['DISCOUNT_PERCENT_POSITION'])) {
-        foreach (explode('-', $arParams['DISCOUNT_PERCENT_POSITION']) as $pos) {
-            $discountPositionClass .= isset($positionClassMap[$pos]) ? ' ' . $positionClassMap[$pos] : '';
-        }
+$templateLibrary = array('popup', 'ajax', 'fx');
+$currencyList = '';
+
+if (!empty($arResult['CURRENCIES'])) {
+    $templateLibrary[] = 'currency';
+    $currencyList = CUtil::PhpToJSObject($arResult['CURRENCIES'], false, true, true);
+}
+
+$templateData = array(
+    'TEMPLATE_LIBRARY' => $templateLibrary,
+    'CURRENCIES' => $currencyList
+);
+unset($currencyList, $templateLibrary);
+
+$elementEdit = CIBlock::GetArrayByID($arParams['IBLOCK_ID'], 'ELEMENT_EDIT');
+$elementDelete = CIBlock::GetArrayByID($arParams['IBLOCK_ID'], 'ELEMENT_DELETE');
+$elementDeleteParams = array('CONFIRM' => GetMessage('CT_BCS_TPL_ELEMENT_DELETE_CONFIRM'));
+
+$positionClassMap = array(
+    'left' => 'product-item-label-left',
+    'center' => 'product-item-label-center',
+    'right' => 'product-item-label-right',
+    'bottom' => 'product-item-label-bottom',
+    'middle' => 'product-item-label-middle',
+    'top' => 'product-item-label-top'
+);
+
+$discountPositionClass = '';
+if ($arParams['SHOW_DISCOUNT_PERCENT'] === 'Y' && !empty($arParams['DISCOUNT_PERCENT_POSITION'])) {
+    foreach (explode('-', $arParams['DISCOUNT_PERCENT_POSITION']) as $pos) {
+        $discountPositionClass .= isset($positionClassMap[$pos]) ? ' ' . $positionClassMap[$pos] : '';
+    }
+}
+
+$labelPositionClass = '';
+if (!empty($arParams['LABEL_PROP_POSITION'])) {
+    foreach (explode('-', $arParams['LABEL_PROP_POSITION']) as $pos) {
+        $labelPositionClass .= isset($positionClassMap[$pos]) ? ' ' . $positionClassMap[$pos] : '';
+    }
+}
+
+$arParams['~MESS_BTN_BUY'] = $arParams['~MESS_BTN_BUY'] ?: Loc::getMessage('CT_BCS_TPL_MESS_BTN_BUY');
+$arParams['~MESS_BTN_DETAIL'] = $arParams['~MESS_BTN_DETAIL'] ?: Loc::getMessage('CT_BCS_TPL_MESS_BTN_DETAIL');
+$arParams['~MESS_BTN_COMPARE'] = $arParams['~MESS_BTN_COMPARE'] ?: Loc::getMessage('CT_BCS_TPL_MESS_BTN_COMPARE');
+$arParams['~MESS_BTN_SUBSCRIBE'] = $arParams['~MESS_BTN_SUBSCRIBE'] ?: Loc::getMessage('CT_BCS_TPL_MESS_BTN_SUBSCRIBE');
+$arParams['~MESS_BTN_ADD_TO_BASKET'] = $arParams['~MESS_BTN_ADD_TO_BASKET'] ?: Loc::getMessage('CT_BCS_TPL_MESS_BTN_ADD_TO_BASKET');
+$arParams['~MESS_NOT_AVAILABLE'] = $arParams['~MESS_NOT_AVAILABLE'] ?: Loc::getMessage('CT_BCS_TPL_MESS_PRODUCT_NOT_AVAILABLE');
+$arParams['~MESS_SHOW_MAX_QUANTITY'] = $arParams['~MESS_SHOW_MAX_QUANTITY'] ?: Loc::getMessage('CT_BCS_CATALOG_SHOW_MAX_QUANTITY');
+$arParams['~MESS_RELATIVE_QUANTITY_MANY'] = $arParams['~MESS_RELATIVE_QUANTITY_MANY'] ?: Loc::getMessage('CT_BCS_CATALOG_RELATIVE_QUANTITY_MANY');
+$arParams['~MESS_RELATIVE_QUANTITY_FEW'] = $arParams['~MESS_RELATIVE_QUANTITY_FEW'] ?: Loc::getMessage('CT_BCS_CATALOG_RELATIVE_QUANTITY_FEW');
+
+$arParams['MESS_BTN_LAZY_LOAD'] = $arParams['MESS_BTN_LAZY_LOAD'] ?: Loc::getMessage('CT_BCS_CATALOG_MESS_BTN_LAZY_LOAD');
+
+$generalParams = array(
+    'SHOW_DISCOUNT_PERCENT' => $arParams['SHOW_DISCOUNT_PERCENT'],
+    'PRODUCT_DISPLAY_MODE' => $arParams['PRODUCT_DISPLAY_MODE'],
+    'SHOW_MAX_QUANTITY' => $arParams['SHOW_MAX_QUANTITY'],
+    'RELATIVE_QUANTITY_FACTOR' => $arParams['RELATIVE_QUANTITY_FACTOR'],
+    'MESS_SHOW_MAX_QUANTITY' => $arParams['~MESS_SHOW_MAX_QUANTITY'],
+    'MESS_RELATIVE_QUANTITY_MANY' => $arParams['~MESS_RELATIVE_QUANTITY_MANY'],
+    'MESS_RELATIVE_QUANTITY_FEW' => $arParams['~MESS_RELATIVE_QUANTITY_FEW'],
+    'SHOW_OLD_PRICE' => $arParams['SHOW_OLD_PRICE'],
+    'USE_PRODUCT_QUANTITY' => $arParams['USE_PRODUCT_QUANTITY'],
+    'PRODUCT_QUANTITY_VARIABLE' => $arParams['PRODUCT_QUANTITY_VARIABLE'],
+    'ADD_TO_BASKET_ACTION' => $arParams['ADD_TO_BASKET_ACTION'],
+    'ADD_PROPERTIES_TO_BASKET' => $arParams['ADD_PROPERTIES_TO_BASKET'],
+    'PRODUCT_PROPS_VARIABLE' => $arParams['PRODUCT_PROPS_VARIABLE'],
+    'SHOW_CLOSE_POPUP' => $arParams['SHOW_CLOSE_POPUP'],
+    'DISPLAY_COMPARE' => $arParams['DISPLAY_COMPARE'],
+    'COMPARE_PATH' => $arParams['COMPARE_PATH'],
+    'COMPARE_NAME' => $arParams['COMPARE_NAME'],
+    'PRODUCT_SUBSCRIPTION' => $arParams['PRODUCT_SUBSCRIPTION'],
+    'PRODUCT_BLOCKS_ORDER' => $arParams['PRODUCT_BLOCKS_ORDER'],
+    'LABEL_POSITION_CLASS' => $labelPositionClass,
+    'DISCOUNT_POSITION_CLASS' => $discountPositionClass,
+    'SLIDER_INTERVAL' => $arParams['SLIDER_INTERVAL'],
+    'SLIDER_PROGRESS' => $arParams['SLIDER_PROGRESS'],
+    '~BASKET_URL' => $arParams['~BASKET_URL'],
+    '~ADD_URL_TEMPLATE' => $arResult['~ADD_URL_TEMPLATE'],
+    '~BUY_URL_TEMPLATE' => $arResult['~BUY_URL_TEMPLATE'],
+    '~COMPARE_URL_TEMPLATE' => $arResult['~COMPARE_URL_TEMPLATE'],
+    '~COMPARE_DELETE_URL_TEMPLATE' => $arResult['~COMPARE_DELETE_URL_TEMPLATE'],
+    'TEMPLATE_THEME' => $arParams['TEMPLATE_THEME'],
+    'USE_ENHANCED_ECOMMERCE' => $arParams['USE_ENHANCED_ECOMMERCE'],
+    'DATA_LAYER_NAME' => $arParams['DATA_LAYER_NAME'],
+    'BRAND_PROPERTY' => $arParams['BRAND_PROPERTY'],
+    'MESS_BTN_BUY' => $arParams['~MESS_BTN_BUY'],
+    'MESS_BTN_DETAIL' => $arParams['~MESS_BTN_DETAIL'],
+    'MESS_BTN_COMPARE' => $arParams['~MESS_BTN_COMPARE'],
+    'MESS_BTN_SUBSCRIBE' => $arParams['~MESS_BTN_SUBSCRIBE'],
+    'MESS_BTN_ADD_TO_BASKET' => $arParams['~MESS_BTN_ADD_TO_BASKET'],
+    'MESS_NOT_AVAILABLE' => $arParams['~MESS_NOT_AVAILABLE'],
+    'PRICE_CODE' => $arParams['PRICE_CODE'],
+    'FILL_ITEM_ALL_PRICES' => 'Y'
+);
+
+$obName = 'ob' . preg_replace('/[^a-zA-Z0-9_]/', 'x', $this->GetEditAreaId($navParams['NavNum']));
+$containerName = 'container-' . $navParams['NavNum'];
+
+$themeClass = isset($arParams['TEMPLATE_THEME']) ? ' bx-' . $arParams['TEMPLATE_THEME'] : '';
+
+
+$fUser = CSaleBasket::GetBasketUserID();
+$arBasketItems = array();
+$dbBasketItems = CSaleBasket::GetList(
+    array("NAME" => "ASC", "ID" => "ASC"),
+    array("FUSER_ID" => $fUser, "LID" => SITE_ID, "ORDER_ID" => "NULL"),
+    false,
+    false,
+    array("ID", "PRODUCT_ID", "QUANTITY",)
+);
+while ($arItems = $dbBasketItems->Fetch()) {
+    if (strlen($arItems["CALLBACK_FUNC"]) > 0) {
+        CSaleBasket::UpdatePrice($arItems["ID"],
+            $arItems["CALLBACK_FUNC"],
+            $arItems["MODULE"],
+            $arItems["PRODUCT_ID"],
+            $arItems["QUANTITY"]);
+        $arItems = CSaleBasket::GetByID($arItems["ID"]);
     }
 
-    $labelPositionClass = '';
-    if (!empty($arParams['LABEL_PROP_POSITION'])) {
-        foreach (explode('-', $arParams['LABEL_PROP_POSITION']) as $pos) {
-            $labelPositionClass .= isset($positionClassMap[$pos]) ? ' ' . $positionClassMap[$pos] : '';
-        }
-    }
-
-    $arParams['~MESS_BTN_BUY'] = $arParams['~MESS_BTN_BUY'] ?: Loc::getMessage('CT_BCS_TPL_MESS_BTN_BUY');
-    $arParams['~MESS_BTN_DETAIL'] = $arParams['~MESS_BTN_DETAIL'] ?: Loc::getMessage('CT_BCS_TPL_MESS_BTN_DETAIL');
-    $arParams['~MESS_BTN_COMPARE'] = $arParams['~MESS_BTN_COMPARE'] ?: Loc::getMessage('CT_BCS_TPL_MESS_BTN_COMPARE');
-    $arParams['~MESS_BTN_SUBSCRIBE'] = $arParams['~MESS_BTN_SUBSCRIBE'] ?: Loc::getMessage('CT_BCS_TPL_MESS_BTN_SUBSCRIBE');
-    $arParams['~MESS_BTN_ADD_TO_BASKET'] = $arParams['~MESS_BTN_ADD_TO_BASKET'] ?: Loc::getMessage('CT_BCS_TPL_MESS_BTN_ADD_TO_BASKET');
-    $arParams['~MESS_NOT_AVAILABLE'] = $arParams['~MESS_NOT_AVAILABLE'] ?: Loc::getMessage('CT_BCS_TPL_MESS_PRODUCT_NOT_AVAILABLE');
-    $arParams['~MESS_SHOW_MAX_QUANTITY'] = $arParams['~MESS_SHOW_MAX_QUANTITY'] ?: Loc::getMessage('CT_BCS_CATALOG_SHOW_MAX_QUANTITY');
-    $arParams['~MESS_RELATIVE_QUANTITY_MANY'] = $arParams['~MESS_RELATIVE_QUANTITY_MANY'] ?: Loc::getMessage('CT_BCS_CATALOG_RELATIVE_QUANTITY_MANY');
-    $arParams['~MESS_RELATIVE_QUANTITY_FEW'] = $arParams['~MESS_RELATIVE_QUANTITY_FEW'] ?: Loc::getMessage('CT_BCS_CATALOG_RELATIVE_QUANTITY_FEW');
-
-    $arParams['MESS_BTN_LAZY_LOAD'] = $arParams['MESS_BTN_LAZY_LOAD'] ?: Loc::getMessage('CT_BCS_CATALOG_MESS_BTN_LAZY_LOAD');
-
-    $generalParams = array(
-        'SHOW_DISCOUNT_PERCENT' => $arParams['SHOW_DISCOUNT_PERCENT'],
-        'PRODUCT_DISPLAY_MODE' => $arParams['PRODUCT_DISPLAY_MODE'],
-        'SHOW_MAX_QUANTITY' => $arParams['SHOW_MAX_QUANTITY'],
-        'RELATIVE_QUANTITY_FACTOR' => $arParams['RELATIVE_QUANTITY_FACTOR'],
-        'MESS_SHOW_MAX_QUANTITY' => $arParams['~MESS_SHOW_MAX_QUANTITY'],
-        'MESS_RELATIVE_QUANTITY_MANY' => $arParams['~MESS_RELATIVE_QUANTITY_MANY'],
-        'MESS_RELATIVE_QUANTITY_FEW' => $arParams['~MESS_RELATIVE_QUANTITY_FEW'],
-        'SHOW_OLD_PRICE' => $arParams['SHOW_OLD_PRICE'],
-        'USE_PRODUCT_QUANTITY' => $arParams['USE_PRODUCT_QUANTITY'],
-        'PRODUCT_QUANTITY_VARIABLE' => $arParams['PRODUCT_QUANTITY_VARIABLE'],
-        'ADD_TO_BASKET_ACTION' => $arParams['ADD_TO_BASKET_ACTION'],
-        'ADD_PROPERTIES_TO_BASKET' => $arParams['ADD_PROPERTIES_TO_BASKET'],
-        'PRODUCT_PROPS_VARIABLE' => $arParams['PRODUCT_PROPS_VARIABLE'],
-        'SHOW_CLOSE_POPUP' => $arParams['SHOW_CLOSE_POPUP'],
-        'DISPLAY_COMPARE' => $arParams['DISPLAY_COMPARE'],
-        'COMPARE_PATH' => $arParams['COMPARE_PATH'],
-        'COMPARE_NAME' => $arParams['COMPARE_NAME'],
-        'PRODUCT_SUBSCRIPTION' => $arParams['PRODUCT_SUBSCRIPTION'],
-        'PRODUCT_BLOCKS_ORDER' => $arParams['PRODUCT_BLOCKS_ORDER'],
-        'LABEL_POSITION_CLASS' => $labelPositionClass,
-        'DISCOUNT_POSITION_CLASS' => $discountPositionClass,
-        'SLIDER_INTERVAL' => $arParams['SLIDER_INTERVAL'],
-        'SLIDER_PROGRESS' => $arParams['SLIDER_PROGRESS'],
-        '~BASKET_URL' => $arParams['~BASKET_URL'],
-        '~ADD_URL_TEMPLATE' => $arResult['~ADD_URL_TEMPLATE'],
-        '~BUY_URL_TEMPLATE' => $arResult['~BUY_URL_TEMPLATE'],
-        '~COMPARE_URL_TEMPLATE' => $arResult['~COMPARE_URL_TEMPLATE'],
-        '~COMPARE_DELETE_URL_TEMPLATE' => $arResult['~COMPARE_DELETE_URL_TEMPLATE'],
-        'TEMPLATE_THEME' => $arParams['TEMPLATE_THEME'],
-        'USE_ENHANCED_ECOMMERCE' => $arParams['USE_ENHANCED_ECOMMERCE'],
-        'DATA_LAYER_NAME' => $arParams['DATA_LAYER_NAME'],
-        'BRAND_PROPERTY' => $arParams['BRAND_PROPERTY'],
-        'MESS_BTN_BUY' => $arParams['~MESS_BTN_BUY'],
-        'MESS_BTN_DETAIL' => $arParams['~MESS_BTN_DETAIL'],
-        'MESS_BTN_COMPARE' => $arParams['~MESS_BTN_COMPARE'],
-        'MESS_BTN_SUBSCRIBE' => $arParams['~MESS_BTN_SUBSCRIBE'],
-        'MESS_BTN_ADD_TO_BASKET' => $arParams['~MESS_BTN_ADD_TO_BASKET'],
-        'MESS_NOT_AVAILABLE' => $arParams['~MESS_NOT_AVAILABLE'],
-        'PRICE_CODE' => $arParams['PRICE_CODE'],
-        'FILL_ITEM_ALL_PRICES' => 'Y'
-    );
-
-    $obName = 'ob' . preg_replace('/[^a-zA-Z0-9_]/', 'x', $this->GetEditAreaId($navParams['NavNum']));
-    $containerName = 'container-' . $navParams['NavNum'];
-
-    $themeClass = isset($arParams['TEMPLATE_THEME']) ? ' bx-' . $arParams['TEMPLATE_THEME'] : '';
-
-
-    $fUser = CSaleBasket::GetBasketUserID();
-    $arBasketItems = array();
-    $dbBasketItems = CSaleBasket::GetList(
-        array("NAME" => "ASC", "ID" => "ASC"),
-        array("FUSER_ID" => $fUser, "LID" => SITE_ID, "ORDER_ID" => "NULL"),
-        false,
-        false,
-        array("ID", "PRODUCT_ID", "QUANTITY",)
-    );
-    while ($arItems = $dbBasketItems->Fetch()) {
-        if (strlen($arItems["CALLBACK_FUNC"]) > 0) {
-            CSaleBasket::UpdatePrice($arItems["ID"],
-                $arItems["CALLBACK_FUNC"],
-                $arItems["MODULE"],
-                $arItems["PRODUCT_ID"],
-                $arItems["QUANTITY"]);
-            $arItems = CSaleBasket::GetByID($arItems["ID"]);
-        }
-
-        $arBasketItems[$arItems["PRODUCT_ID"]] = $arItems["QUANTITY"];
-    }
+    $arBasketItems[$arItems["PRODUCT_ID"]] = $arItems["QUANTITY"];
+}
 // Печатаем массив, содержащий актуальную на текущий момент корзину
 
-    global $arrFilterTop;
-    $arrFilterTop = array();
+global $arrFilterTop;
+$arrFilterTop = array();
 
-    $basketUserId = (int)$fUser;
-    if ($basketUserId <= 0) {
-        $ids = array();
-    }
-    $ids = array_values(Catalog\CatalogViewedProductTable::getProductSkuMap(
-        IBLOCK_CATALOG,
-        $arResult['VARIABLES']['SECTION_ID'],
-        $basketUserId,
-        $arParams['SECTION_ELEMENT_ID'],
-        $arParams['PAGE_ELEMENT_COUNT'],
-        $arParams['DEPTH']
-    ));
+$basketUserId = (int)$fUser;
+if ($basketUserId <= 0) {
+    $ids = array();
+}
+$ids = array_values(Catalog\CatalogViewedProductTable::getProductSkuMap(
+    IBLOCK_CATALOG,
+    $arResult['VARIABLES']['SECTION_ID'],
+    $basketUserId,
+    $arParams['SECTION_ELEMENT_ID'],
+    $arParams['PAGE_ELEMENT_COUNT'],
+    $arParams['DEPTH']
+));
 
-    $arrFilterTop['ID'] = $ids;
+$arrFilterTop['ID'] = $ids;
 
 // получение лайков и избранного для всех элементов каталога НАЧАЛО
 
-    $id_USER = $USER->GetID();
-    $FUser_id = Fuser::getId($id_USER);
-    $item_id = $prop_see_in_window = [];
+$id_USER = $USER->GetID();
+$FUser_id = Fuser::getId($id_USER);
+$item_id = $prop_see_in_window = [];
 
-    foreach ($arResult['ITEMS'] as $item) {
-        $item_id[] = $item['ID'];
+foreach ($arResult['ITEMS'] as $item) {
+    $item_id[] = $item['ID'];
+}
+
+$iblock_id = IBLOCK_CATALOG;
+$resQuery = Enterego\EnteregoSettings::getPropSetting($iblock_id, 'SEE_POPUP_WINDOW');
+if (!empty($resQuery)) {
+    while ($collectionPropChecked = $resQuery->Fetch()) {
+        $prop_see_in_window[$collectionPropChecked['CODE']] = $collectionPropChecked;
     }
+}
 
-    $iblock_id = IBLOCK_CATALOG;
-    $resQuery = Enterego\EnteregoSettings::getPropSetting($iblock_id, 'SEE_POPUP_WINDOW');
-    if (!empty($resQuery)) {
-        while ($collectionPropChecked = $resQuery->Fetch()) {
-            $prop_see_in_window[$collectionPropChecked['CODE']] = $collectionPropChecked;
-        }
-    }
-
-    $count_likes = DataBase_like::getLikeFavoriteAllProduct($item_id, $FUser_id);
+$count_likes = DataBase_like::getLikeFavoriteAllProduct($item_id, $FUser_id);
 // получение лайков и избранного для всех элементов каталога КОНЕЦ
 
 
