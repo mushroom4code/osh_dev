d83bbbd8f75e4a9313ac7460218f83a98302f367
maxim.vagatkin <vagatkin@enterego.ru>
2023-08-04 19:26:11 +0300

    #000018806 доработки отображения акций

--- local/components/bitrix/enterego.discounts.list/templates/.default/template.php
+++ local/components/bitrix/enterego.discounts.list/templates/.default/template.php
@@ -12,42 +12,42 @@ $curDateTime = new \Bitrix\Main\Type\DateTime();
         осуществляется.
         Сайт предназначен для потребителей старше 18 лет.</p>
     <div class="d-flex flex-column mt-4 mb-5" id="box_discounts">
-        <div class="box_with_discounts_parents justify-content-between" id="click_discount_s">
+        <div class="box_with_discounts_parents <?= count($arResult['DISCOUNTS']) <= 2 ? 'discounts_flex' : 'discounts_grid'?>
+         justify-content-between" id="click_discount_s">
             <?php foreach ($arResult['DISCOUNTS'] as $iblock_key => $discount) {
                 $discount_iblock = $discount['DISCOUNT_IBLOCK'];
+
+                if ($discount['ACTIVE'] != 'Y') {
+                    $discount_status = 'не активна';
+                } else {
+                    if (!empty($discount['ACTIVE_TO'])) {
+                        if (!empty($discount['ACTIVE_FROM'])
+                            && $discount['ACTIVE_FROM'] <= $curDateTime) {
+                            if ($discount['ACTIVE_TO'] >= $curDateTime) {
+                                $discount_status = 'до ' . $discount['ACTIVE_TO']->format('d.m.Y');
+                            } else {
+                                $discount_status = 'завершена';
+                            }
+                        } else {
+                            $discount_status = 'еще не запущена';
+                        }
+                    } else {
+                        $discount_status = 'бессрочно';
+                    }
+                }
                 ?>
+
                 <div class="box_with_discounts d-flex justify-content-center align-items-center col col-sm">
                     <a href="/akcii/<?= $discount_iblock['CODE'] ?>/" class="logo_discount">
                         <?php if ($discount_iblock['PICTURE']) {
                             $iblockImage = CFile::GetByID($discount_iblock['PICTURE'])->fetch(); ?>
-                            <img src="<?= $iblockImage['SRC'] ?>">
+                            <img src="<?= $iblockImage['SRC'] ?>" class="<?= $discount_status == 'завершена' ? 'completed_discount' : ''?>">
                         <?php } else { ?>
-                            <img src="/local/templates/Oshisha/images/no-photo.gif" class="discount_no_image">
+                            <img src="/local/templates/Oshisha/images/no-photo.gif" class="discount_no_image <?= $discount_status == 'завершена' ? 'completed_discount' : ''?>">
                         <?php } ?>
                         <div class="discount_info_box">
                             <div><?= $discount['NAME'] ?></div>
-                            <div>
-                                <?php
-                                if ($discount['ACTIVE'] != 'Y') {
-                                    echo 'не активна';
-                                } else {
-                                    if (!empty($discount['ACTIVE_TO'])) {
-                                        if (!empty($discount['ACTIVE_FROM'])
-                                            && $discount['ACTIVE_FROM'] <= $curDateTime) {
-                                            if ($discount['ACTIVE_TO'] >= $curDateTime) {
-                                                echo 'до ' . $discount['ACTIVE_TO']->format('d.m.Y');
-                                            } else {
-                                                echo 'завершена';
-                                            }
-                                        } else {
-                                            echo 'еще не запущена';
-                                        }
-                                    } else {
-                                        echo 'бессрочно';
-                                    }
-                                }
-                                ?>
-                            </div>
+                            <div><?= $discount_status ?></div>
                         </div>
                     </a>
                 </div>
