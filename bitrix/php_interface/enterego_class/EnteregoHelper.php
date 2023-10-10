fde64c09b5bdbf55a8179029ca4254d321d1a921
maxim.vagatkin <vagatkin@enterego.ru>
2023-10-10 16:07:11 +0300

    #000020579 проверка на подарок через свойство

--- bitrix/php_interface/enterego_class/EnteregoHelper.php
+++ bitrix/php_interface/enterego_class/EnteregoHelper.php
@@ -188,7 +188,7 @@ class EnteregoHelper
      */
     public static function productIsGift(int $productId): bool
     {
-        $rsRes = CIBlockElement::GetList([], ['ID' => $productId, 'SECTION_ID' => CATALOG_GIFT_ID]);
+        $rsRes = CIBlockElement::GetList([], ['ID' => $productId, 'PROPERTY_'.IS_MERCH_PROPERTY => 'Да']);
         return $rsRes->SelectedRowsCount() > 0;
     }
 }
