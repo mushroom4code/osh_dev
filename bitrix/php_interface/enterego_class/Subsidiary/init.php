bfb8b33e674e04d6c5eb81b78ccacb37dd8d2c12
erodionova <rodionova@enterego.ru>
2023-11-13 15:56:18 +0300

    #000021171 - ошибка передачи сайт ид раньше перехвата файлом редактирования

--- bitrix/php_interface/enterego_class/Subsidiary/init.php
+++ bitrix/php_interface/enterego_class/Subsidiary/init.php
@@ -1,12 +1,15 @@
 <?php
 
 require_once __DIR__ . '/conf.php';
+if ($_SERVER['SCRIPT_NAME'] === '/bitrix/admin/public_file_edit.php') {
+    return;
+}
 
 if (defined('SUBSIDIARY_ENABLE') && SUBSIDIARY_ENABLE && !defined('SITE_ID'))
 {
     session_start();
-    $siteId = $_SESSION['subsidiary'] ?? 'N2';
-    if (!empty($siteId && in_array($siteId, SUBSIDIARY_SITE_LIST))) {
-        define('SITE_ID', $siteId);
+    $_siteId = $_SESSION['subsidiary'] ?? 'N2';
+    if (!empty($_siteId && in_array($_siteId, SUBSIDIARY_SITE_LIST))) {
+        define('SITE_ID', $_siteId);
     }
 }
