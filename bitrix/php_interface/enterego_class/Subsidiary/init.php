3fa8fa48f65218467880a86b71346511cace1fd1
Karpenko Andrey <ak@enterego.ru>
2024-01-09 17:26:42 +0300

    #000021594 - add fill block relation with site

--- bitrix/php_interface/enterego_class/Subsidiary/init.php
+++ bitrix/php_interface/enterego_class/Subsidiary/init.php
@@ -1,5 +1,10 @@
 <?php
 
+use Bitrix\Iblock\IblockSiteTable;
+use Bitrix\Main\ArgumentException;
+use Bitrix\Main\ObjectPropertyException;
+use Bitrix\Main\SystemException;
+
 require_once __DIR__ . '/conf.php';
 if ($_SERVER['SCRIPT_NAME'] === '/bitrix/admin/public_file_edit.php') {
     return;
@@ -12,4 +17,41 @@ if (defined('SUBSIDIARY_ENABLE') && SUBSIDIARY_ENABLE && !defined('SITE_ID'))
     if (!empty($_siteId && in_array($_siteId, SUBSIDIARY_SITE_LIST))) {
         define('SITE_ID', $_siteId);
     }
+}
+
+/** set relation for new subsidiary (iblock)
+ * execute from bitrix php console
+ *
+ * @param $mainSite string
+ * @param $subSite string
+ * @return bool
+ * @throws ArgumentException
+ * @throws ObjectPropertyException
+ * @throws SystemException
+ */
+function addNewSubsidiary(string $mainSite, string $subSite): bool
+{
+    if (!CModule::IncludeModule('iblock')) {
+        return false;
+    }
+
+    $arMainBlock = [];
+    $rsMainBlock =  IblockSiteTable::getList(['filter'=> ['SITE_ID'=>$mainSite]]);
+    while ($iBlock = $rsMainBlock->fetch()) {
+        $arMainBlock[] =$iBlock['IBLOCK_ID'];
+    }
+
+    $arSubBlock = [];
+    $rsSubBlock =  IblockSiteTable::getList(['filter'=> ['SITE_ID'=>$subSite]]);
+    while ($iBlock = $rsSubBlock->fetch()) {
+        $arSubBlock[] =$iBlock['IBLOCK_ID'];
+    }
+
+    foreach ($arMainBlock as $itemMainBlock) {
+        if (!in_array($itemMainBlock, $arSubBlock)) {
+            IblockSiteTable::add(['IBLOCK_ID'=>$itemMainBlock, 'SITE_ID'=>$subSite]);
+        }
+    }
+
+    return true;
 }
