98a88830752c89b5d1e3239c7aaf012a85ac7776
erodionova <rodionova@enterego.ru>
2024-02-12 16:51:12 +0300

    #000021881 - починка блока оплат

--- local/templates/Oshisha/assets/app/components/order_page/OrderPaySystems.js
+++ local/templates/Oshisha/assets/app/components/order_page/OrderPaySystems.js
@@ -1,28 +1,27 @@
-import React, { useContext, useRef } from "react";
+import React, {useContext} from "react";
 import OrderContext from "./Context/OrderContext";
 
-function OrderPayItem({ payment, selectPaySystem }) {
+function OrderPayItem({payment, selectPaySystem}) {
     return (
-        <div className={`bg-textDark rounded-lg min-h-[170px] dark:bg-darkBox md:w-2/5 md:mr-4 mr-0 w-full
-                ${payment.CHECKED === 'Y' ? 'bx-selected border-2 dark:border border-solid border-light-red ' +
+        <div className={`bg-textDark rounded-lg min-h-[170px] h-auto dark:bg-darkBox md:w-2/5 md:mr-4 mb-4 mr-0 w-full
+                ${payment.CHECKED === 'Y' ? 'bx-selected border dark:border border-solid border-hover-red ' +
             'dark:border-grey-line-order bg-white' : ''}`}
-            onClick={ () => {
-                if (payment.CHECKED === 'Y') {
-                    return
-                }
-                selectPaySystem(payment.ID)}
-            }>
+             onClick={() => {
+                 if (payment.CHECKED === 'Y') {
+                     return
+                 }
+                 selectPaySystem(payment.ID) }
+             }>
             <div className="p-8 flex w-full cursor-pointer">
                 <input type="checkbox" id={'ID_PAY_SYSTEM_ID_' + payment.ID}
-                    name="PAY_SYSTEM_ID"
-                    className="bx-soa-pp-company-checkbox hidden" value={payment.ID}
-                    defaultChecked={payment.CHECKED === 'Y'} />
+                       name="PAY_SYSTEM_ID"
+                       className="bx-soa-pp-company-checkbox hidden" value={payment.ID}
+                       defaultChecked={payment.CHECKED === 'Y'}/>
                 <div className="text-base w-full">
-                    <p className={`font-medium dark:font-normal mt-2 mb-2.5 dark:text-white
-                     ${payment.CHECKED === 'Y' ? 'text-light-red' : 'text-black'}`}>
+                    <p className={'font-medium dark:font-normal mt-2 mb-2.5 dark:text-white'}>
                         {payment.NAME}
                     </p>
-                    <div className="text-stone-600 text-sm dark:text-gray-300">{payment.DESCRIPTION}</div>
+                    <div className="text-stone-600 text-xs dark:text-gray-300">{payment.DESCRIPTION}</div>
                 </div>
             </div>
         </div>
@@ -30,12 +29,10 @@ function OrderPayItem({ payment, selectPaySystem }) {
 }
 
 function OrderPaySystems() {
-    const { result, sendRequest } = useContext(OrderContext);
+    const {result, sendRequest} = useContext(OrderContext);
 
     const selectPaySystem = (paymentId) => {
-        BX.OrderPageComponents.startLoader();
-
-        sendRequest('refreshOrderAjax', { PAY_SYSTEM_ID: paymentId }, { PAY_SYSTEM_ID: paymentId });
+        sendRequest('refreshOrderAjax', {}, {PAY_SYSTEM_ID: paymentId});
     }
 
     return (
@@ -44,8 +41,9 @@ function OrderPaySystems() {
             <div className="bx-soa-pp">
                 <div className='flex flex-wrap flex-row'>
                     {
-                        result.PAY_SYSTEM.map(payment => <OrderPayItem key={'paysystem_block_' + payment.ID}
-                                                                       payment={payment} selectPaySystem={selectPaySystem} />)
+                        result.PAY_SYSTEM.map(payment =>
+                            <OrderPayItem key={'paysystem_block_' + payment.ID}
+                                          payment={payment} selectPaySystem={selectPaySystem}/>)
                     }
                 </div>
             </div>
