55fe3628b04f691a2c72108f1a45435f4ca6188d
erodionova <rodionova@enterego.ru>
2024-02-29 18:54:29 +0300

    #000021834 - прайс  лист

--- bitrix/php_interface/enterego_class/PriceList.php
+++ bitrix/php_interface/enterego_class/PriceList.php
@@ -1,5 +1,9 @@
 <?php
+
 namespace Enterego;
+
+use CCatalogStore;
+use CIBlockElement;
 use COption;
 use Shuchkin\SimpleXLSXGen;
 
@@ -11,6 +15,7 @@ use Shuchkin\SimpleXLSXGen;
  */
 class PriceList
 {
+
     private $arResult = [];
     private $categories_id;
     private $price_list_dir;
@@ -18,6 +23,7 @@ class PriceList
 
     private $category_type = '';
     private $brand = '';
+
 
     public function __construct()
     {
@@ -39,36 +45,49 @@ class PriceList
 
     private function update()
     {
+        $arStore = $storages = [];
+        $StoragesQuery = CCatalogStore::GetList(
+            array('PRODUCT_ID' => 'ASC', 'ID' => 'ASC'),
+            array('ACTIVE' => 'Y', 'TITLE' => 'Москва'),
+            false,
+            false,
+            array("ID", "TITLE")
+        );
+
+        $ar = $StoragesQuery->Fetch();
+        if (!empty($ar)) {
+            $storages[$ar['ID']] = $ar;
+            $storages['TITLE'][] = 'Склад ' . $ar['TITLE'];
+            $arStore[] = "CATALOG_STORE_AMOUNT_" . $ar['ID'];
+        }
 
         $this->arResult[] = ['<b style="font-size: 25px;font-weight:bold; ">ПРАЙС-ЛИСТ OSHISHA.NET - 8 (499) 350-62-01</b>'];
         $this->arResult[] = ['<b>Дата формирования</b>', '<b>' . date('d.m.y') . '</b>'];
         $this->arResult[] = [];
-        $this->arResult[] = [
+        $this->arResult[] = array_merge([
             'Группа товара',
             'Бренд',
             'Наименование',
             'Заказ штук',
-            'b2b',
-            'от 10 до 30 тыс.руб',
-            'от 30 тыс. руб',
-            'ссылка на продукт'];
+            'b2b/₽',
+            'ссылка на продукт'], $storages['TITLE']);
 
 
         $tree = \CIBlockSection::GetTreeList(array('DEPTH_LEVEL' => 1, 'ACTIVE' => 'Y'), array('ID', 'NAME'));
         while ($category = $tree->GetNext()) {
             if (in_array($category['ID'], $this->categories_id)) {
-                $category['child'] = $this->childrenCategory($category['ID']);
+                $category['child'] = $this->childrenCategory($category['ID'], $arStore);
 
                 $this->category_type = $category['NAME'];
                 if (!empty($category['child'])) {
                     foreach ($category['child'] as $key => $child_category) {
-                        $this->createTree($child_category);
+                        $this->createTree($child_category, $arStore);
                     }
                 }
             }
         }
         $xlsx = SimpleXLSXGen::fromArray($this->arResult)->setDefaultFontSize(12);
-        $xlsx->autoFilter('A4:H10000');
+        $xlsx->autoFilter('A4:G10000');
         $file_name = 'price-list-oshisha-' . date("d.m.Y") . '-' . date("H:i:s") . '.xlsx';
         $path_to_file = $this->price_list_dir . $file_name;
         $xlsx->saveAs($path_to_file);
@@ -105,43 +124,64 @@ class PriceList
 
     private function setLog($text)
     {
-        file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/price-list_log.txt', date('d.m.Y') . $text . "\n", 8);
+        file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/price-list_log.txt', date('d.m.Y')
+            . $text . "\n", 8);
     }
 
-    private function childrenCategory($section_id)
+    private function childrenCategory($section_id, $arStore = [])
     {
         $result = array();
-        $tree = \CIBlockSection::GetList(array("NAME" => "ASC", "left_margin" => "asc"), array('ACTIVE' => 'Y', 'SECTION_ID' => $section_id), false, array('ID', 'NAME'));
+
+
+        $tree = \CIBlockSection::GetList(
+            array("NAME" => "ASC", "left_margin" => "asc"),
+            array('ACTIVE' => 'Y', 'SECTION_ID' => $section_id),
+            false,
+            array('ID', 'NAME')
+        );
+
         while ($section = $tree->GetNext()) {
             $prod = array();
-            $section['child'] = $this->childrenCategory($section['ID']);
+            $section['child'] = $this->childrenCategory($section['ID'], $arStore);
 
-            $arSelect = array(
+            $arSelect = array_merge($arStore, array(
                 "ID",
                 'IBLOCK_ID',
                 "NAME",
-                "catalog_PRICE_" . RETAIL_PRICE,
-                "catalog_PRICE_" . BASIC_PRICE,
-                "catalog_PRICE_" . B2B_PRICE,
+                "PRICE_" . B2B_PRICE,
                 "CODE",
-                "PROPERTY_MARKIROVANNYY"
-            );
-            $arFilter = array("IBLOCK_SECTION_ID" => IntVal($section['ID']), "ACTIVE" => "Y", "CATALOG_AVAILABLE" => "Y", ">=CATALOG_QUANTITY" => 1);
+                "PROPERTY_MARKIROVANNYY",
+            ));
+
+            $arFilter = [
+                "IBLOCK_SECTION_ID" => IntVal($section['ID']),
+                "ACTIVE" => "Y",
+                "CATALOG_AVAILABLE" => "Y",
+                ">=CATALOG_QUANTITY" => 1];
+
             $res = CIBlockElement::GetList(array("NAME" => "ASC"), $arFilter, false, array(), $arSelect);
             while ($arRes = $res->Fetch()) {
                 $base_price = \CPrice::GetBasePrice($arRes['ID']);
                 $base_price = !empty($base_price) ? $base_price['PRICE'] : '';
 
-                $prod[] = array(
+                $StoreAr = [];
+                foreach ($arStore as $store) {
+                    if (!empty($arRes[$store])) {
+                        $StoreAr[$store] = $arRes[$store];
+                    } else {
+                        $StoreAr[$store] = 0;
+                    }
+                }
+
+                $prod[] = array_merge(array(
                     'NAME' => $arRes['NAME'],
                     'PATH' => 'https://' . OSHISHA_DOMAIN . '/catalog/product/' . $arRes['CODE'] . '/'
                         ?? 'https://' . OSHISHA_DOMAIN,
                     'BASE' => $base_price,
-                    'DO_10' => $arRes['CATALOG_PRICE_' . RETAIL_PRICE],
-                    'OT_10_DO_30' => $arRes['CATALOG_PRICE_' . BASIC_PRICE],
-                    'OT_30' => $arRes['CATALOG_PRICE_' . B2B_PRICE],
-                    'USE_MARKING' => $arRes['PROPERTY_MARKIROVANNYY_VALUE']
-                );
+                    'b2b' => $arRes['PRICE_' . B2B_PRICE],
+                    'USE_MARKING' => $arRes['PROPERTY_MARKIROVANNYY_VALUE'],
+
+                ), $StoreAr);
                 unset($section['LEFT_MARGIN'], $section['~LEFT_MARGIN'], $section['~NAME'], $section['~ID']);
             }
             $section['products'] = $prod;
@@ -153,41 +193,43 @@ class PriceList
 
     /**
      * @param $child
+     * @param $arStore
      */
-    private function createTree($child)
+    private function createTree($child, $arStore)
     {
         if (!empty($child['products'])) {
             $this->brand = $child['NAME'];
 
             foreach ($child['products'] as $product_item) {
-                if (empty($product_item['BASE']) || empty($product_item['DO_10']) ||
-                    empty($product_item['OT_10_DO_30']) || empty($product_item['OT_30'])) continue;
+                if (empty($product_item['BASE']) || empty($product_item['b2b'])) continue;
 
                 $product_item['BASE'] = (int)$product_item['BASE'];
-                $product_item['DO_10'] = (int)$product_item['DO_10'];
-                $product_item['OT_10_DO_30'] = (int)$product_item['OT_10_DO_30'];
-                $product_item['OT_30'] = (int)$product_item['OT_30'];
+                $product_item['b2b'] = (int)$product_item['b2b'];
                 $product_name = $product_item['NAME'];
                 if ($product_item['USE_MARKING'] === 'Да') {
                     $product_name = $product_item['NAME'] . ' МРК';
                 }
+                $storage = [];
+                foreach ($product_item as $key => $value) {
+                    if (in_array($key, $arStore)) {
+                        $storage[] = $value;
+                    }
+                }
 
-                $this->arResult[] = [
+                $this->arResult[] = array_merge([
                     $this->category_type,
                     $this->brand,
                     $product_name,
                     ' ',
-                    $product_item['DO_10'],
-                    $product_item['OT_10_DO_30'],
-                    $product_item['OT_30'],
-                    '<a href="' . $product_item['PATH'] . '">Ссылка на товар</a>'
-                ];
+                    $product_item['b2b'],
+                    '<a href="' . $product_item['PATH'] . '">Ссылка на товар</a>',
+                ], $storage);
             }
         }
 
         if (!empty($child['child'])) {
             foreach ($child['child'] as $ch) {
-                $this->createTree($ch);
+                $this->createTree($ch, $arStore);
             }
         }
     }
